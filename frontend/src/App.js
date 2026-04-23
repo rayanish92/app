4ae4410fdf115a64207bed7eb22b2b74fe020ec4
rayanish92 <rayanish92@gmail.com>
2026-04-24 01:19:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import { Toaster } from './components/ui/sonner';
 import MobileLayout from './components/MobileLayout';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Farmers from './pages/Farmers';
+import Farmers from './pages/Farmers'; // <-- Correctly imported
 import Bills from './pages/Bills';
 import Payments from './pages/Payments';
 import SMS from './pages/SMS';
@@ -34,7 +34,8 @@ function App() {
           >
             <Route index element={<Navigate to="/dashboard" replace />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="farmers" element={<Consumers />} />
+            {/* FIX: Changed element={<Consumers />} to element={<Farmers />} */}
+            <Route path="farmers" element={<Farmers />} />
             <Route path="bills" element={<Bills />} />
             <Route path="payments" element={<Payments />} />
             <Route path="sms" element={<SMS />} />
